b290e3e3c465335511ccd0953fe4cef1a56f33f0
shikkandar <mohammedshikkandar1@gmail.com>
2023-12-08 15:21:18 +0530

    update 2.1

--- src/component/front/Home.jsx
+++ src/component/front/Home.jsx
@@ -99,7 +99,7 @@
             </div>
           ) : null}
           {isShowRemove.length !== 0 ? (
-            <div data-aos='slide-left' data-aos-duration="600" className="alert-mesage p-2 text-center  fw-bold d-flex justify-content-arround align-items-center" style={{backgroundColor:'#F6A6A3',color:'#7E4A4D'}}>
+            <div data-aos='slide-left' data-aos-duration="600" className="alert-mesage p-2 text-center  fw-bold d-flex justify-content-arround align-items-center" style={{backgroundColor:'#F6A6A3',color:'#90120E'}}>
                 <div>{isShowRemove} Removed in cart</div><svg className='alert-svg' xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path fill="#cd7570" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
               <div className="progress-line " style={{backgroundColor:'#90120E'}}></div>
             </div>
